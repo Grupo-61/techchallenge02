55ce7cf1ec23c749ba95a3aab5824fe6e0c6875b
brunogabrieldeoliveira <brunogabrieldeoliveira@gmail.com>
2025-07-08 21:49:25 -0300

    Ingestão de dados no S3 como boto3

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from pathlib import Path
 import pandas as pd
 from datetime import datetime
 from scrap import obtemDadosB3
+import boto3
+import os
+from dotenv import load_dotenv
+
+_= load_dotenv()
+
+session = boto3.Session(
+    aws_access_key_id= os.getenv('aws_access_key_id'),
+    aws_secret_access_key= os.getenv('aws_secret_access_key'),
+    aws_session_token= os.getenv('aws_session_token'), 
+    region_name= os.getenv('region_name')
+)
+
+s3 = session.client('s3')
+
+# listar buckets
+# response = s3.list_buckets()
+# for bucket in response['Buckets']:
+#     print(bucket['Name'])
 
 # obtenho dados b3
 dados, colunas= obtemDadosB3()
@@ -10,11 +29,32 @@ df = pd.DataFrame(dados, columns=colunas)
 # crio sub-dir data
 subdir= str(datetime.now()).split(" ")[0] 
 
-# logica boto3 
-
 # exporto .parquet
-destino= "dados/" + subdir
+# destino= "dados/" + subdir
+destino= subdir
 Path(destino).mkdir(parents=True, exist_ok=True)
 
 #Salvo em .parquet
-df.to_parquet(f'{destino}/dados.parquet', engine='pyarrow', index=False)
+destino= f'{destino}/dados.parquet'
+df.to_parquet(destino, engine='pyarrow', index=False)
+
+# testo scrap
+arquivo= Path(destino)
+
+print(f'Arquivo: {arquivo}')
+
+if arquivo.exists():
+
+    # bucket destino
+    bucket_name= os.getenv('s3_bucket_destino')
+    s3_key= destino
+    arquivo_local= arquivo
+
+    # Fazer o upload
+    s3.upload_file(arquivo_local, bucket_name, s3_key)
+    print("Upload realizado com sucesso!")
+
+else:
+    print("Arquivo não encontrado!")
+
+
